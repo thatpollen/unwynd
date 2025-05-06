302f643dccf7b330be7711b6c100410826dd8e57
diamond <diamonddewan05@gmail.com>
2025-05-07 02:59:09 +0600

    refactor: remove secret key string

--- src/app/api/common/configs.ts
+++ src/app/api/common/configs.ts
@@ -4,7 +4,7 @@ const config = {
     timeout: 5000,
     logLevel: process.env.LOG_LEVEL || 'info',
     stripeAccountId: process.env.STRIPE_ACCOUNT_ID || 'acct_123456789',
-    stripeApiKey: process.env.NODE_ENV === 'production' ? process.env.STRIPE_API_KEY_PROD ||'sk_live_' : process.env.STRIPE_API_KEY_PROD || 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
+    stripeApiKey: process.env.NODE_ENV === 'production' ? process.env.STRIPE_API_KEY_PROD ||'sk_live_' : process.env.STRIPE_API_KEY_PROD || 'sk_test_',
 };
 
 export default config;
